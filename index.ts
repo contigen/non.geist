68ec96396e3194f11aef9e5eeff86744fa3d2061
contigen <69436315+contigen@users.noreply.github.com>
2023-11-26 13:24:22 +0100

    Add a watcher for code in dev

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Hello via Bun!");
+import { join } from 'path'
+import { processFiles, processSingleFile } from './utils'
+import chokidar from 'chokidar'
+
+const FONTS_DIR = `/fonts`
+const SANS_STATIC_DIR = `/sans/static`
+const MONO_STATIC_DIR = `/mono/static`
+const sansDir = join(import.meta.dir, FONTS_DIR, SANS_STATIC_DIR)
+const monoDir = join(import.meta.dir, FONTS_DIR, MONO_STATIC_DIR)
+
+processFiles(sansDir)
+processFiles(monoDir)
+console.log(`running`)
+// if (process.env.NODE_ENV) {
+//   chokidar
+//     .watch([sansDir, monoDir], { ignoreInitial: true })
+//     .on(`add`, (evt, path) => {
+//       path?.isFile() && processSingleFile(evt)
+//     })
+// }
