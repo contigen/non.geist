33d4c3e020590c5a57a089666a48523e33da5f0b
contigen <69436315+contigen@users.noreply.github.com.>
2026-08-07 09:00:35 +0100

    Add pixel directory processing to file watcher

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,18 +4,22 @@ import { processFiles, processSingleFile } from './utils'
 const FONTS_DIR = `fonts`
 const SANS_STATIC_DIR = `/sans/static`
 const MONO_STATIC_DIR = `/mono/static`
+const PIXEL_DIR = `/pixel`
 const sansDir = join(FONTS_DIR, SANS_STATIC_DIR)
 const monoDir = join(FONTS_DIR, MONO_STATIC_DIR)
+const pixelDir = join(FONTS_DIR, PIXEL_DIR)
 
 processFiles(sansDir)
 processFiles(monoDir)
+processFiles(pixelDir)
 
 if (process.env.NODE_ENV === `development`) {
   import(`chokidar`).then((chokidar) => {
     chokidar
-      .watch([sansDir, monoDir], { ignoreInitial: true })
+      .watch([sansDir, monoDir, pixelDir], { ignoreInitial: true })
       .on(`add`, (evt, path) => {
         path?.isFile() && processSingleFile(evt)
       })
   })
 }
+
